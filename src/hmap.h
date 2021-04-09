4f46790b64d735c7391cef9c995220cd10d0b662
aklapatch <klapatchaaron@gmail.com>
2021-04-09 00:12:56 -0400

    Got the hash function working and it seems to work well.

--- src/hmap.h
+++ src/hmap.h
@@ -18,75 +18,80 @@
 #define C_DS_AHASH_MULTIPLE (6364136223846793005)
 #endif
 
-uint64_t wrapping_mul(uint64_t a, uint64_t b){
+uint64_t ahash_wrapping_mul(uint64_t a, uint64_t b){
     return (a*b) % UINT64_MAX;
 }
-uint64_t wrapping_add(uint64_t a, uint64_t b){
+uint64_t ahash_wrapping_add(uint64_t a, uint64_t b){
     return (a+b) % UINT64_MAX;
 }
 
-uint64_t rotr(uint64_t n, int32_t c){
+uint64_t ahash_rotr(uint64_t n, int32_t c){
     uint32_t mask = (8*sizeof(n) - 1);
     c &= mask;
     return (n << c) | (n>> ( (-c)&mask));
 }
 
-uint64_t rotl(uint64_t n, int32_t c){
+uint64_t ahash_rotl(uint64_t n, int32_t c){
     uint32_t mask = (8*sizeof(n) - 1);
     c &= mask;
     return (n >> c) | (n << ( (-c)&mask));
 }
 
-void update(uint64_t * buf, uint64_t * pad, uint64_t data_in){
-    uint64_t tmp = wrapping_mul( (data_in ^ *buf), C_DS_AHASH_MULTIPLE );
-    *pad = wrapping_mul(rotl((*pad ^ tmp), 8) , C_DS_AHASH_MULTIPLE);
-    *buf = rotl((*buf ^ *pad), 24);
+void ahash_update(uint64_t * buf, uint64_t * pad, uint64_t data_in){
+    uint64_t tmp = ahash_wrapping_mul( (data_in ^ *buf), C_DS_AHASH_MULTIPLE );
+    *pad = ahash_wrapping_mul(ahash_rotl((*pad ^ tmp), 8) , C_DS_AHASH_MULTIPLE);
+    *buf = ahash_rotl((*buf ^ *pad), 24);
 }
 
-void update_128(uint64_t * buf, uint64_t * pad, uint64_t data_in[2]){
-    update(buf, pad, data_in[0]);
-    update(buf, pad, data_in[1]);
+void ahash_update_128(uint64_t * buf, uint64_t * pad, uint64_t data_in[2]){
+    ahash_update(buf, pad, data_in[0]);
+    ahash_update(buf, pad, data_in[1]);
 }
 
 uint64_t ahash_buf(uint8_t *data, size_t data_len){
     uint64_t buffer = C_DS_HASH_SEED1, pad = C_DS_HASH_SEED2;
 
-    buffer = wrapping_mul(C_DS_AHASH_MULTIPLE, wrapping_add((uint64_t)data_len, buffer));
+    buffer = ahash_wrapping_mul(C_DS_AHASH_MULTIPLE, ahash_wrapping_add((uint64_t)data_len, buffer));
 
     if (data_len > 8){
         if (data_len > 16){
             // update on the last 128 bits
-            uint64_t *last_128 = (uint64_t*)(&data[data_len - 17]);
-            update_128(&buffer, &pad, last_128);
+            uint64_t last_128[2];
+            memcpy(&last_128, &data[data_len - 17], sizeof(last_128));
+            ahash_update_128(&buffer, &pad, last_128);
             while (data_len > 16){
-                update(&buffer, &pad, *(uint64_t*)data);
+                memcpy(last_128, data, sizeof(last_128[0]));
+                ahash_update(&buffer, &pad, last_128[0]);
                 data += sizeof(uint64_t);
                 data_len -= sizeof(uint64_t);
             }
         } else {
-            update(&buffer, &pad, *(uint64_t*)data);
-            update(&buffer, &pad, *(uint64_t*)&data[data_len - 9]);
+            uint64_t first, last;
+            memcpy(&first, data, sizeof(first));
+            memcpy(&last, data + data_len - 9, sizeof(last));
+            ahash_update(&buffer, &pad, first);
+            ahash_update(&buffer, &pad, last);
         }
     }else{
         uint64_t vals[2] = {0};
         if (data_len >= 2){
             if (data_len >= 4){
-                vals[0] = *(uint32_t*)data;
-                vals[1] = *(uint32_t*)&data[data_len - 5];
+                memcpy(vals, data, 4);
+                memcpy(&vals[1], data + data_len - 5, 4);
             } else {
-                vals[0] = *(uint16_t*)data;
-                vals[1] = *(uint16_t*)&data[data_len - 3];
+                memcpy(vals, data, 2);
+                memcpy(&vals[1], data + data_len - 3, 2);
             }
         } else {
             if (data_len > 0){
                 vals[1] = vals[0] = data[0];
             } 
         }
-        update_128(&buffer, &pad, vals);
+        ahash_update_128(&buffer, &pad, vals);
     }
 
     uint32_t rot = buffer & 63;
-    return rotl(wrapping_mul(C_DS_AHASH_MULTIPLE, buffer) ^ pad, rot);
+    return ahash_rotl(ahash_wrapping_mul(C_DS_AHASH_MULTIPLE, buffer) ^ pad, rot);
 }
 
 #endif
--- src/hmap_test.c
+++ src/hmap_test.c
@@ -1,6 +1,30 @@
 #include"hmap.h"
+#define ROUNDS (4*UINT16_MAX)
 
 int main(){
+
+    uint64_t matches[ROUNDS] = {0};
+    uint32_t total_different_vals = 0;
+    uint32_t collisions = 0;
+    for (uint64_t i = 0; i < ROUNDS; ++i){
+        bool collision_found = false;
+        uint64_t hash = ahash_buf((uint8_t*)&i, sizeof(i));
+
+        // see if any other matches have been found yet
+        for (uint32_t i = 0; i < total_different_vals; ++i){
+            if (hash == matches[i]){
+                printf("Collision found! %lx\n", hash);
+                collision_found = true;
+                collisions++;
+                break;
+            }
+        }
+        if (!collision_found){
+            matches[total_different_vals] = hash;
+            total_different_vals++;
+        }
+    }
+    printf("Total collisions: %u\n", collisions);
 
     return 0;
 }
