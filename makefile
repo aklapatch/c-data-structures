776262a1c334aa90ec5f76454045d52e4096afcd
aklapatch <klapatchaaron@gmail.com>
2021-11-26 20:59:56 -0500

    Cleaned up some test stuff and some un-needed files.

--- makefile
+++ makefile
@@ -1,8 +1,6 @@
 CFLAGS=-ggdb -Wall -Wextra
 OUTDIR=./build/
 
-tests: dynarr hmap
-
 hmap: src/hmap.h src/hmap_test.c src/test_helpers.h
 	$(CC) $(CFLAGS) src/hmap_test.c -o $(OUTDIR)/hmap_test
 
@@ -16,10 +14,6 @@ dynarr: src/dynarr_test.c src/test_helpers.h src/dynarr.h
 dynarr_test: dynarr
 	$(OUTDIR)/dynarr_test
 
-j_dist: src/jump_dist_test.c
-	$(CC) $(CFLAGS) src/jump_dist_test.c -o $(OUTDIR)/jump_dist_test
-	$(OUTDIR)/jump_dist_test
-
 outdir:
 	mkdir -p $(OUTDIR)
 
@@ -29,8 +23,4 @@ clean:
 hmap_test: hmap
 	$(OUTDIR)/hmap_test
 
-test: tests
-	$(OUTDIR)/dynarr_test
-	echo "Starting hash_test"
-	echo "---------------------------------------------------------------------"
-	$(OUTDIR)/hash_test
+tests: dynarr_test hmap_test  hash_test
--- src/test_helpers.h
+++ src/test_helpers.h
@@ -3,30 +3,12 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define ITEMS_IN_ARR(arr) (sizeof((arr))/sizeof((arr)[0]))
 #define TESTGROUP(label) printf("[Test Group]: %s\n", label);
-#define TEST(label, expression)\
-    do{\
-        if (!(expression)){\
-            printf("[Test]: (%s) FAIL! (%s) is false @ %s:%u\n",label, #expression, __FILE__, __LINE__);\
-            exit(1);\
-        }\
-    } while(0)
 
-// This macro passing to a function is for a specific reason.
 // We only want the expression (val1 or val2) to be evaluated once, but
 // we still want to get the value of the result of the function.
 // We also want the stringified version of the expression, which 
 // We can only get from a macro.
-// I guess I could use variables, and that may be better.
-// The values are in the funciton, so they should be printed there
-// To associate the value with the string, pass the string into the function
-#define TESTINTNEQ(val1, val2)\
-    do{\
-        if (test_int_eq((val1), (val2), #val1, #val2, __FILE__, __LINE__)){  exit(1); }\
-    }while(0)
-
-
 #define TESTINTEQ(val1, val2)\
     do{\
         uintptr_t _val1 = (val1), _val2 = (val2);\
@@ -36,7 +18,7 @@
         }\
     }while(0)
 
-// C, reducing code duplication by casting pointers to integers
+// ahhhh C, reducing code duplication by casting pointers to integers
 // (sarcastic)
 #define TESTPTREQ(ptr1, ptr2) TESTINTEQ((uintptr_t)ptr1, (uintptr_t)ptr2)
 
@@ -50,15 +32,3 @@
     }while(0)
 
 #define TESTPTRNEQ(ptr1, ptr2) TESTINTNEQ((uintptr_t)ptr1, (uintptr_t)ptr2)
-
-#define TESTERRSUCCESS(message, ptr, is_hm)\
-    do{\
-        TEST(message " err val check", is_hm ? hm_err(ptr) == ds_success : dynarr_err(ptr) == ds_success);\
-        TEST(message " err set check", is_hm ? !hm_is_err_set(ptr) : !dynarr_is_err_set(ptr));\
-    }while (0)
-
-#define TESTERRFAIL(message, ptr, err_val, is_hm)\
-    do{\
-        TEST(message " err val check", is_hm ? hm_err(ptr) == err_val : dynarr_err(ptr) == err_val);\
-        TEST(message " err set check", is_hm ? hm_is_err_set(ptr) : dynarr_is_err_set(ptr));\
-    }while (0)
