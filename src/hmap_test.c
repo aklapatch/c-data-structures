9454ce75f19472566028f34bd2af61d84414df34
aklapatch <klapatchaaron@gmail.com>
2021-11-27 22:14:48 -0500

    Cleaned up some test variables.

--- src/hmap_test.c
+++ src/hmap_test.c
@@ -3,6 +3,7 @@
 #include "test_helpers.h"
 #include <stdlib.h>
 
+#define NUM_KEYS (32)
 int main(){
     
     uint16_t *hmap = NULL;
@@ -14,55 +15,50 @@ int main(){
     TEST_INT_EQ(hm_cap(hmap), 32);
 
     // insert a lot of values and see how this goes.
-    uint16_t ins_vals[31];
-    uint64_t keys[31];
     TEST_GROUP("Insert a few keys");
-    for (uint32_t i = 0; i < sizeof(ins_vals)/sizeof(ins_vals[0]); ++i){
-        ins_vals[i] = i;
-        keys[i] = i;
+    for (uint32_t i = 0; i < NUM_KEYS; ++i){
         // insert the value
-        printf("key=%lu\n", i);
-        hm_set(hmap, keys[i], ins_vals[i]);
+        hm_set(hmap, i, i);
         TEST_INT_EQ(hm_err(hmap), ds_success);
 
         uint16_t out_val = UINT16_MAX;
-        hm_get(hmap, keys[i], out_val);
+        hm_get(hmap, i, out_val);
         TEST_INT_EQ(hm_err(hmap), ds_success);
-        TEST_INT_EQ(out_val, ins_vals[i]);
+        TEST_INT_EQ(out_val, i);
     }
 
     // make sure all values didn't get overwritten
-    for (uint32_t i = 0; i < sizeof(ins_vals)/sizeof(ins_vals[0]); ++i){
+    for (uint32_t i = 0; i < NUM_KEYS; ++i){
         uint16_t out_val = UINT16_MAX;
-        hm_get(hmap, keys[i], out_val);
+        hm_get(hmap, i, out_val);
         TEST_INT_EQ(hm_err(hmap), ds_success);
-        TEST_INT_EQ(out_val, ins_vals[i]);
+        TEST_INT_EQ(out_val, i);
     }
 
     TEST_GROUP("Realloc key preservation");
     hm_realloc(hmap, 64);
     // try resizing the hmap and see if all the key are still there
-    for (uint32_t i = 0; i < sizeof(ins_vals)/sizeof(ins_vals[0]); ++i){
+    for (uint32_t i = 0; i < NUM_KEYS; ++i){
         uint16_t out_val = UINT16_MAX;
-        hm_get(hmap, keys[i], out_val);
+        hm_get(hmap, i, out_val);
         TEST_INT_EQ(hm_err(hmap), ds_success);
-        TEST_INT_EQ(out_val, ins_vals[i]);
+        TEST_INT_EQ(out_val, i);
     }
 
     // try deleting all the keys and make sure they're gone
     TEST_GROUP("Ensure deletion");
-    for (uint32_t i = 0; i < sizeof(ins_vals)/sizeof(ins_vals[0]); ++i){
+    for (uint32_t i = 0; i < NUM_KEYS; ++i){
         // insert the value
         // removing key 3 causes a chain of moves that I'm still debuggin
         // removing 3 seems to work fine
         // 11 is on a list with 3 and 9, but should be at bucket 3 slot 2
         // it may be getting moved to a slot where it does not belong, which is weird
         // I need to implement direct hit tracking to fix this I think.
-        hm_del(hmap, keys[i]);
+        hm_del(hmap, i);
         TEST_INT_EQ(hm_err(hmap), ds_success);
 
         uint16_t out_val = UINT16_MAX;
-        hm_get(hmap, keys[i], out_val);
+        hm_get(hmap, i, out_val);
         TEST_INT_EQ(hm_err(hmap), ds_not_found);
     }
 
