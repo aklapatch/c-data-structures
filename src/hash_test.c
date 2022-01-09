d6effeddc7a371e28cf5256cbea4673c13141e73
Aaron Klapatch <sniper96a@gmail.com>
2022-01-09 14:44:32 -0500

    Added xxhash function, it's not better for small data compared to ahash.

--- src/hash_test.c
+++ src/hash_test.c
@@ -1,19 +1,20 @@
 #include "ahash.h"
+#include "xxhash.h"
 #include <stdio.h>  
+#include <time.h>
 #define ROUNDS (1*UINT16_MAX)
 
 int main(){
-    // test distribution of the ahash function
-
-    uint64_t start_val = 0xf32341234;
-    uint64_t test_val = ahash_buf((uint8_t*)&start_val, sizeof(start_val));
-    uint64_t final_second_hash = ahash_buf((uint8_t*)&test_val, sizeof(test_val));
-    printf("test_val = %lx final_second_hash = %lx\n", test_val, final_second_hash);
-
     uint64_t hashes[ROUNDS] = {0};
+
+    printf("Starting ahash test\n");
+
+    clock_t time = clock();
     for (uint64_t i = 0; i < ROUNDS; ++i){
-        hashes[i] = ahash_buf((uint8_t*)&i, sizeof(i));
+        hashes[i] = ahash_buf(&i, sizeof(i));
     }
+    time = clock() - time;
+    printf("%u 8 bytes hashes took %g sec %lu clocks\n", ROUNDS , (double)(time)/CLOCKS_PER_SEC, time);
     for (uint64_t i = 0; i < ROUNDS; ++i){
         //compare different matches and see if one was found
         for (uint64_t j = 0; j < ROUNDS; ++j){
@@ -23,6 +24,26 @@ int main(){
             }
         }
     }
+
+    printf("Starting xxhash test\n");
+
+    time = clock();
+    for (uint64_t i = 0; i < ROUNDS; ++i){
+        hashes[i] = xxhash_buf(&i, sizeof(i));
+    }
+    time = clock() - time;
+    printf("%u 8 bytes hashes took %g sec %lu clocks\n", ROUNDS , (double)(time)/CLOCKS_PER_SEC, time);
+
+    for (uint64_t i = 0; i < ROUNDS; ++i){
+        //compare different matches and see if one was found
+        for (uint64_t j = 0; j < ROUNDS; ++j){
+            if (j != i && hashes[i] == hashes[j]){
+                printf("Collision found! %lx i = %lu j = %lu\n", hashes[i], i,j);
+                return 1;
+            }
+        }
+    }
+
 
     return 0;
 }
--- src/xxhash.h
+++ src/xxhash.h
@@ -3,29 +3,69 @@
 #include <stdint.h>
 
 // inspired by create.stephan-brumme.com/xxhash/#sourcecode
+//
+// Use uintptr_t so that on a 64bit arch it's xxhash 64, and on a 32 bit arch it's xxhahs32
 
-#define XXHASH_SEED_1 (0x3141592653589793)
+#define XXHASH_SEED_1 (0x31415926)
 
 uintptr_t rot_left(uintptr_t n, uint8_t num){
-    return (n << num) | (n >> (32 - num));
-}
-
-uintptr_t process_one(uintptr_t prev, uintptr_t in, uintptr_t prime2, uintptr_t prime1){
-    return rot_left(prev + in * prime2, ) * prime1;
-
+    return (n << num) | (n >> ((sizeof(uintptr_t) * 8) - num));
 }
 
 uintptr_t xxhash_buf(void *data, size_t data_len){
     const uintptr_t primes[] = { 2654435761U, 22468225119U, 3266489917U, 668265263U, 374761393U};
-    uintptr_t state[4] = {0};
+    uintptr_t state[4] = {
+        XXHASH_SEED_1 + primes [0] + primes[1],
+        XXHASH_SEED_1 + primes[1],
+        XXHASH_SEED_1,
+        XXHASH_SEED_1 - primes[0]
+    };
+    uint8_t state_num = sizeof(state)/sizeof(state[0]);
 
-    // seed the initial state
-    state[0] = XXHASH_SEED_1 + primes[0] + primes[1];
-    state[1] = XXHASH_SEED_1 + prime[1];
-    state[2] = XXHASH_SEED_1;
-    state[3] = XXHASH_SEED_1 - prime[0];
+    uint8_t ptr64 = sizeof(uintptr_t) >= sizeof(uint64_t);
 
-    uintptr_t result = 
+    // main processing loop, 4 ptr-sized bytes at a time, data should hopefully be aligned
+    uint8_t shift_amt = ptr64 ? 13 : 31;
+    uintptr_t *block = (uintptr_t*)data;
+    size_t tmp_len = data_len;
+    for (; tmp_len >= sizeof(state); tmp_len -= sizeof(state),block += state_num){
+        for (uint8_t i = 0; i < sizeof(state)/sizeof(state[0]); ++i){
+            state[i] = rot_left(state[i] + block[i] * primes[1], shift_amt) * primes[0];
+        }
+    }
 
+    uintptr_t result = data_len;
+    if (data_len >= sizeof(state)){
+        uint8_t shifts[] = { 1, 7, 12, 18 };
+        for (uint8_t i = 0; i < state_num; ++i){
+            result += rot_left(state[i], shifts[i]);
+        }
+        if (ptr64) {
+            for(uint8_t i = 0; i < state_num; ++i){
+                uintptr_t tmp = rot_left(state[i] * primes[1], 31) * primes[0];
+                result = (result ^ tmp) * primes[0] + primes[3];
+            }
+        }
+    } else {
+        result += state[2] + primes[4];
+    }
 
+    // hash one uintptr_t at a time until we get to byte interval sizes
+    for(; tmp_len > sizeof(uintptr_t); tmp_len -= sizeof(uintptr_t), ++block){
+        result = rot_left(result + *block * primes[2], ptr64 ? 31 : 17);
+    }
+
+    uint8_t *byte_data = (uint8_t*)block;
+    for (; tmp_len > 0; --tmp_len, ++byte_data){
+        result = rot_left(result + *byte_data * primes[4], 11);
+    }
+
+    uint8_t uint_half_bits = sizeof(uintptr_t)*4;
+    result ^= result >> (uint_half_bits - 1);
+    result *= primes[1];
+    result ^= result >> (uint_half_bits - 3);
+    result *= primes[2];
+    result ^= result >> uint_half_bits;
+
+    return result;
 }
